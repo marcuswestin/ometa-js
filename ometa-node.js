6eaf2f991c478eaca9d684a6475714b083e2d30a
Marcus Westin <marcus.westin@gmail.com>
2010-02-25 16:04:02 -0800

    Make ometa-node usable for the browser as well

--- ometa-node.js
+++ ometa-node.js
@@ -5,9 +5,6 @@ require("./bs-js-compiler")
 require("./bs-ometa-compiler")
 require("./bs-ometa-optimizer")
 require("./bs-ometa-js-compiler")
-var sys = require('sys')
-
-alert = print
 
 translateCode = function(s) {
   var translationError = function(m, i) { alert("Translation error - please tell Alex about this!"); throw fail },
